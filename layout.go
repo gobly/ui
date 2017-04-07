58ffe0a4bf5fb9ca67390979cd01ac408ff674ad
The Ranger <ranger@risk.ee>
2017-04-07 19:23:13 +0300

    Move module directory detection to core module

--- layout.go
+++ layout.go
@@ -3,39 +3,26 @@ package ui
 import (
 	"html/template"
 	"path"
-	"path/filepath"
-	"strings"
 	"github.com/gobly/core"
 )
-
-// Package path depends on the caller, this cannot be determined on loading
-var packagePath = func() string {
-	pkgPath, err := filepath.Rel(core.App.Root, core.CallerPath(3))
-	if err != nil {
-		panic("Cannot determine package path from " + pkgPath)
-	}
-
-	// Get first component from pkgPath, and prepend absolute path of the source directory
-	return filepath.Join(core.App.Root, strings.Split(filepath.ToSlash(pkgPath), "/")[0])
-}
 
 var funcMap = template.FuncMap{
 	"App" : func() core.Application { return core.App },
 }
 
 func LoadSingle(mainTemplate string) *template.Template {
-	mainTemplate = path.Join(packagePath(), mainTemplate)
-	footerTemplate := path.Join(core.CurrentPath("ui"), "html", "footer.html")
-	singleTemplate := path.Join(core.CurrentPath("ui"), "html", "single.html")
+	mainTemplate = path.Join(core.CurrentModule(), mainTemplate)
+	footerTemplate := path.Join(core.CurrentFolder(), "html", "footer.html")
+	singleTemplate := path.Join(core.CurrentFolder(), "html", "single.html")
 
 	return template.Must(template.New("single.html").Funcs(funcMap).ParseFiles(singleTemplate, mainTemplate, footerTemplate))
 }
 
 func LoadDouble(mainTemplate string, sidebarTemplate string) *template.Template {
-	mainTemplate = path.Join(packagePath(), mainTemplate)
-	sidebarTemplate = path.Join(packagePath(), sidebarTemplate)
-	footerTemplate := path.Join(core.CurrentPath("ui"), "html", "footer.html")
-	doubleTemplate := path.Join(core.CurrentPath("ui"), "html", "double.html")
+	mainTemplate = path.Join(core.CurrentModule(), mainTemplate)
+	sidebarTemplate = path.Join(core.CurrentModule(), sidebarTemplate)
+	footerTemplate := path.Join(core.CurrentFolder(), "html", "footer.html")
+	doubleTemplate := path.Join(core.CurrentFolder(), "html", "double.html")
 
 	return template.Must(template.New("double.html").Funcs(funcMap).ParseFiles(doubleTemplate, mainTemplate, sidebarTemplate, footerTemplate))
 }
